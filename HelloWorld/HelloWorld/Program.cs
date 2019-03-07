411cd0547807769c09843cd0e6f153aacebe4919
anguyen <anguyen@arst.com.au>
2019-03-07 17:43:34 +1100

    blah

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld {
+	class Program {
+		
+		public static void Main(string[] args) {
+			int num1;
+			int num2;
+			string inputStorage1;
+			string inputStorage2;
+			
+			inputStorage1 = Console.ReadLine();
+			inputStorage2 = Console.ReadLine();
+			
+			num1 = int.Parse(inputStorage1);
+			num2 = int.Parse(inputStorage2);
+			
+			int sum = num1 + num2;
+			
+			Console.WriteLine(sum);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
